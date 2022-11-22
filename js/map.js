6a8fe69bf341bccb65300da65acd229e6c66081e
u7425 <112527671+u7425@users.noreply.github.com>
2022-11-21 18:37:05 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,38 +1,38 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 41.871193, lng: -87.618983},
+    center: {lat: 41.880193, lng: -87.633866},
     zoom: 13,
   });
+
+  var marker1 = new google.maps.Marker({
+    position: {lat: 41.871193, lng: -87.618983},
+    map: map,
+  });
+
+  var text1 = "The best spot for Frisbee";
+
+  var info1 = new google.maps.InfoWindow({
+    content: text1,
+  });
+
+  google.maps.event.addListener (marker1, 'click', function() {
+    info1.open(map, marker1);
+  });
+
+  var marker2 = new google.maps.Marker({
+    position: {lat: 41.885250, lng: -87.651030},
+    map: map,
+  });
+
+  var text2 = "My favorite food market!";
+
+  var info2 = new google.maps.InfoWindow({
+    content: text2,
+  });
+
+  google.maps.event.addListener (marker2, 'click', function() {
+    info2.open(map, marker2);
+  });
 };
-
-var marker1 = new google.maps.Marker({
-  position: {lat: 41.871193, lng: -87.618983},
-  map: map,
-});
-
-var text1 = "The best spot for Frisbee";
-
-var info1 = new google.maps.InfoWindow({
-  content: text1,
-});
-
-google.maps.event.addListener (marker1, 'click', function() {
-  info1.open(map, marker1);
-});
-
-var marker2 = new google.maps.Marker({
-  position: {lat: 41.885250, lng: -87.651030},
-  map: map,
-});
-
-var text2 = "My favorite food market!";
-
-var info2 = new google.maps.InfoWindow({
-  content: text2,
-});
-
-google.maps.event.addListener (marker2, 'click', function() {
-  info2.open(map, marker2);
-});
 
 window.initMap = initMap;
