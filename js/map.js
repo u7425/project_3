3e45a455ec2ee4a444403bc1b06056ac19140f55
u7425 <112527671+u7425@users.noreply.github.com>
2022-11-22 18:13:20 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -33,6 +33,28 @@ function initMap() {
   google.maps.event.addListener (marker2, 'click', function() {
     info2.open(map, marker2);
   });
+  
+  var circle1 = new google.maps.Circle({
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    map,
+    center: marker1.getPosition(),
+    radius: 200,
+  });
+
+  var circle2 = new google.maps.Circle({
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    map,
+    center: marker2.getPosition(),
+    radius: 200,
+  });
 }
 
 window.initMap = initMap;
